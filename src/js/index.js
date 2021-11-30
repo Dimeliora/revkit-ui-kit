a72ec0fc9ffe27e6fbe4d69db4a14b16a64dc9c5
Dimeliora <dimeliora710@gmail.com>
2021-11-30 16:58:41 +0900

    rating improvements

--- src/js/index.js
+++ src/js/index.js
@@ -5,94 +5,94 @@ import { Progress } from "./progress";
 import { Rating } from "./rating";
 
 const SELECT_ITEMS = [
-	{
-		value: "Item 1",
-	},
-	{
-		value: "Item 2",
-	},
-	{
-		value: "Item 3",
-	},
-	{
-		value: "Item 4",
-	},
+    {
+        value: "Item 1",
+    },
+    {
+        value: "Item 2",
+    },
+    {
+        value: "Item 3",
+    },
+    {
+        value: "Item 4",
+    },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-	new Select({
-		selector: "#form-select-01",
-		data: SELECT_ITEMS,
-	});
-	new Select({
-		selector: "#form-select-02",
-		data: SELECT_ITEMS,
-		selected: 1,
-	});
-	new Select({
-		selector: "#form-select-03",
-		data: SELECT_ITEMS,
-		disabled: true,
-	});
+    new Select({
+        selector: "#form-select-01",
+        data: SELECT_ITEMS,
+    });
+    new Select({
+        selector: "#form-select-02",
+        data: SELECT_ITEMS,
+        selected: 1,
+    });
+    new Select({
+        selector: "#form-select-03",
+        data: SELECT_ITEMS,
+        disabled: true,
+    });
 
-	new Stepper({
-		selector: "#form-stepper-01",
-		value: 1,
-		max: 5,
-	});
-	new Stepper({
-		selector: "#form-stepper-02",
-		value: 1,
-		disabled: true,
-	});
+    new Stepper({
+        selector: "#form-stepper-01",
+        value: 1,
+        max: 5,
+    });
+    new Stepper({
+        selector: "#form-stepper-02",
+        value: 1,
+        disabled: true,
+    });
 
-	new Slider({
-		selector: "#form-slider-01",
-		value: 75,
-	});
-	new Slider({
-		selector: "#form-slider-02",
-		value: 25,
-		disabled: true,
-	});
+    new Slider({
+        selector: "#form-slider-01",
+        value: 75,
+    });
+    new Slider({
+        selector: "#form-slider-02",
+        value: 25,
+        disabled: true,
+    });
 
-	const progressBar_1 = new Progress({
-		selector: "#form-progress-01",
-		value: 60,
-	});
-	const progressBar_2 = new Progress({
-		selector: "#form-progress-02",
-		value: 40,
-		type: "warning",
-	});
-	const progressBar_3 = new Progress({
-		selector: "#form-progress-03",
-		value: 80,
-		type: "success",
-	});
-	const progressBar_4 = new Progress({
-		selector: "#form-progress-04",
-		value: 50,
-		type: "error",
-	});
-	setInterval(() => {
-		const value = Math.floor(Math.random() * 100);
-		progressBar_1.onChange(value);
-	}, 1000);
-	setInterval(() => {
-		const value = Math.floor(Math.random() * 100);
-		progressBar_2.onChange(value);
-	}, 2000);
-	setInterval(() => {
-		const value = Math.floor(Math.random() * 100);
-		progressBar_3.onChange(value);
-	}, 4000);
-	setInterval(() => {
-		const value = Math.floor(Math.random() * 100);
-		progressBar_4.onChange(value);
-	}, 3500);
+    const progressBar_1 = new Progress({
+        selector: "#form-progress-01",
+        value: 60,
+    });
+    const progressBar_2 = new Progress({
+        selector: "#form-progress-02",
+        value: 40,
+        type: "warning",
+    });
+    const progressBar_3 = new Progress({
+        selector: "#form-progress-03",
+        value: 80,
+        type: "success",
+    });
+    const progressBar_4 = new Progress({
+        selector: "#form-progress-04",
+        value: 50,
+        type: "error",
+    });
+    setInterval(() => {
+        const value = Math.floor(Math.random() * 100);
+        progressBar_1.onChange(value);
+    }, 1000);
+    setInterval(() => {
+        const value = Math.floor(Math.random() * 100);
+        progressBar_2.onChange(value);
+    }, 2000);
+    setInterval(() => {
+        const value = Math.floor(Math.random() * 100);
+        progressBar_3.onChange(value);
+    }, 4000);
+    setInterval(() => {
+        const value = Math.floor(Math.random() * 100);
+        progressBar_4.onChange(value);
+    }, 3500);
 
-	new Rating({
-		selector: "#rating-01",
-	});
+    new Rating({
+        selector: "#rating-01",
+    });
 });
--- src/js/rating.js
+++ src/js/rating.js
@@ -1,137 +1,156 @@
 export class Rating {
-	static starItemTemplate(value) {
-		return `
+    static getStarItemTemplate(value) {
+        return `
             <button
                 class="rating__star-item"
                 data-rating-value="${value}"
                 data-rating-element="button-item"
                 aria-label="Rating star button ${value}"
-				tabindex="${value}"
             >
-                <svg
-                    class="rating__star-icon"
-                    viewBox="0 0 20 19"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M10 15L4.12215 18.0902L5.24472 11.5451L0.489435 6.90983L7.06107 5.95492L10 0L12.9389 5.95492L19.5106 6.90983L14.7553 11.5451L15.8779 18.0902L10 15Z"
-                    />
-                </svg>
+				<svg
+					class="rating__star-icon rating__star-icon--left"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						viewBox="0 0 10 20"
+						d="M 10 15 L 4.1222 18.0902 L 5.2447 11.5451 L 0.4894 6.9098 L 7.0611 5.9549 L 10 0 L 10 15 Z"
+					/>
+				</svg>
+				<svg
+					class="rating__star-icon rating__star-icon--right"
+					viewBox="10 0 10 20"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M 10 15 L 10 0 L 12.9389 5.9549 L 19.5106 6.9098 L 14.7553 11.5451 L 15.8779 18.0902 L 10 15 Z"
+					/>
+				</svg>
             </button>
         `;
-	}
+    }
 
-	static createHTMLTemplate() {
-		const starButtons = [...Array(5).keys()]
-			.map((item) => Rating.starItemTemplate(item + 1))
-			.reverse();
+    static createHTMLTemplate() {
+        const starButtons = [...Array(5).keys()]
+            .map((item) => Rating.getStarItemTemplate(item + 1))
+            .reverse();
 
-		return `
+        return `
             <div class="rating__top">
-                <div class="rating__stars" data-rating-element="buttons">
+                <div class="rating__stars">
                     ${starButtons.join(" ")}
                 </div>
-                <div class="rating__value" data-rating-element="value">0.0</div>
+                <div class="rating__value" data-rating-element="value">Nobody rate yet</div>
             </div>
             <div class="rating__bottom" data-rating-element="votes"></div>
         `;
-	}
+    }
 
-	#ratings = 0;
-	#votes = 0;
-	#value = 0;
-	#root;
-	#ratingButtons;
-	#ratingValue;
-	#ratingVotes;
+    #ratings = 0;
+    #votes = 0;
+    #value = 0;
+    #root;
+    #ratingButtons;
+    #ratingValue;
+    #ratingVotes;
 
-	/**
-	 * @param {Object} options - Options object for rating creation
-	 * @param {string} options.selector - CSS selector for rating element injection
-	 */
-	constructor({ selector }) {
-		this.#root = document.querySelector(selector);
-		if (this.#root === null) {
-			console.error(
-				`Container element "${selector}" for rating is not available`
-			);
-			return;
-		}
+    /**
+     * @param {Object} options - Options object for rating creation
+     * @param {string} options.selector - CSS selector for rating element injection
+     */
+    constructor({ selector }) {
+        this.#root = document.querySelector(selector);
+        if (this.#root === null) {
+            console.error(
+                `Container element "${selector}" for rating is not available`
+            );
+            return;
+        }
 
-		this.#render();
-		this.#setup();
-	}
+        this.#render();
+        this.#setup();
+    }
 
-	get value() {
-		return Number(this.#value.toFixed(1));
-	}
+    get value() {
+        return Number(this.#value.toFixed(1));
+    }
 
-	get count() {
-		return this.#votes;
-	}
+    get count() {
+        return this.#votes;
+    }
 
-	#render() {
-		this.#root.classList.add("select");
-		this.#root.innerHTML = Rating.createHTMLTemplate();
-	}
+    #render() {
+        this.#root.classList.add("select");
+        this.#root.innerHTML = Rating.createHTMLTemplate();
+    }
 
-	#setup() {
-		this.#ratingButtons = this.#root.querySelector(
-			'[data-rating-element="buttons"]'
-		);
-		this.#ratingValue = this.#root.querySelector(
-			'[data-rating-element="value"]'
-		);
-		this.#ratingVotes = this.#root.querySelector(
-			'[data-rating-element="votes"]'
-		);
+    #setup() {
+        this.#ratingButtons = this.#root.querySelectorAll(
+            '[data-rating-element="button-item"]'
+        );
+        this.#ratingValue = this.#root.querySelector(
+            '[data-rating-element="value"]'
+        );
+        this.#ratingVotes = this.#root.querySelector(
+            '[data-rating-element="votes"]'
+        );
 
-		this.#ratingButtons.addEventListener(
-			"click",
-			this.#ratingButtonsClickHandler
-		);
-	}
+        this.#ratingButtons.forEach((button) => {
+            button.addEventListener("click", this.#ratingButtonsClickHandler);
+        });
+    }
 
-	#ratingButtonsClickHandler = ({ target }) => {
-		const buttonElm = target.closest('[data-rating-element="button-item"]');
-		if (buttonElm) {
-			const ratingValue = Number(buttonElm.dataset.ratingValue);
-			this.#ratings += ratingValue;
+    #ratingButtonsClickHandler = ({ currentTarget }) => {
+        const ratingButtonValue = Number(currentTarget.dataset.ratingValue);
+        this.#ratings += ratingButtonValue;
 
-			this.#votes += 1;
+        this.#votes += 1;
 
-			this.#value = this.#ratings / this.#votes;
+        this.#value = this.#ratings / this.#votes;
 
-			this.#updateRatingView();
-		}
-	};
+        this.#updateRatingView();
 
-	#updateRatingView() {
-		this.#ratingValue.textContent = this.#value.toFixed(1);
+        currentTarget.blur();
+    };
 
-		const ratingFloor = Math.floor(this.#value);
+    #updateRatingView() {
+        this.#ratingValue.textContent = this.#value.toFixed(1);
 
-		[...this.#ratingButtons.children].forEach((button) => {
-			const ratingValue = Number(button.dataset.ratingValue);
-			if (ratingValue <= ratingFloor) {
-				button.classList.add("rating__star-item--active");
-			} else {
-				button.classList.remove("rating__star-item--active");
-			}
-		});
+        this.#ratingVotes.textContent = `based on ${this.#votes} ratings`;
 
-		this.#ratingVotes.textContent = `based on ${this.#votes} ratings`;
-	}
+        const intRating = Math.floor(this.value);
+        const fracRating = this.value - intRating;
 
-	destroy() {
-		this.#ratingButtons.removeEventListener(
-			"click",
-			this.#ratingButtonsClickHandler
-		);
+        this.#ratingButtons.forEach((button) => {
+            const ratingButtonValue = Number(button.dataset.ratingValue);
 
-		this.#root.classList.remove("select");
-		this.#root.innerHTML = "";
-	}
+            if (ratingButtonValue <= intRating) {
+                button.classList.add("rating__star-item--full");
+            } else {
+                button.classList.remove(
+                    "rating__star-item--full",
+                    "rating__star-item--half"
+                );
+            }
+
+            if (fracRating && ratingButtonValue === intRating + 1) {
+                if (fracRating < 0.5) {
+                    button.classList.add("rating__star-item--half");
+                } else {
+                    button.classList.add("rating__star-item--full");
+                }
+            }
+        });
+    }
+
+    destroy() {
+        this.#ratingButtons.forEach((button) => {
+            button.removeEventListener(
+                "click",
+                this.#ratingButtonsClickHandler
+            );
+        });
+
+        this.#root.classList.remove("select");
+        this.#root.innerHTML = "";
+    }
 }
