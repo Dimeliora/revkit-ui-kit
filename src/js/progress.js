fc05adcbcafc2cf914ddae9bb58bbf06046f3afe
Dimeliora <dimeliora710@gmail.com>
2021-12-25 15:33:24 +0900

    progress component refactoring

--- src/js/progress.js
+++ src/js/progress.js
@@ -1,60 +1,70 @@
 export class Progress {
-	static progressTypeClassnames = {
-		warning: "progress--warning",
-		success: "progress--success",
-		error: "progress--error",
-	};
+  static createHTMLTemplate() {
+    return `
+	  	<div class="progress__filler" data-progress-filler></div>
+	`;
+  }
 
-	
-	#value;
-	#type;
-	#root;
-	
-	/**
-	 * @param {Object} options - Options object for progress element creation
-	 * @param {string} options.selector - CSS selector for progress element injection
-	 * @param {number} [options.value=0] - Initial progress value
-	 * @param {?('warning'|'success'|'error')=} options.type - Progress element styling variant
-	 */
-	constructor({ selector, value = 0, type = null }) {
-		this.#root = document.querySelector(selector);
-		if (this.#root === null) {
-			console.error(
-				`Container element "${selector}" for progress is not available`
-			);
-			return;
-		}
+  static progressTypeClassnames = {
+    warning: "progress--warning",
+    success: "progress--success",
+    error: "progress--error",
+  };
 
-		this.#value = value;
-		this.#type = type;
+  #value;
+  #type;
+  #root;
+  #filler;
 
-		this.#setup();
-	}
+  /**
+   * @param {Object} options - Options object for progress element creation
+   * @param {string} options.selector - CSS selector for progress element injection
+   * @param {number} [options.value=0] - Initial progress value
+   * @param {?('warning'|'success'|'error')=} options.type - Progress element styling variant
+   */
+  constructor({ selector, value = 0, type = null }) {
+    this.#root = document.querySelector(selector);
+    if (this.#root === null) {
+      console.error(
+        `Container element "${selector}" for progress is not available`
+      );
+      return;
+    }
 
-	#setup() {
-		this.#root.classList.add("progress");
-		if (this.#type !== null) {
-			this.#root.classList.add(
-				Progress.progressTypeClassnames[this.#type]
-			);
-		}
+    this.#value = value;
+    this.#type = type;
 
-		this.onChange(this.#value);
-	}
+    this.#render();
+    this.#setup();
+  }
 
-	onChange(value) {
-		if (this.#root !== null) {
-			this.#root.style.setProperty("--progress-fill-width", `${value}%`);
-		}
-	}
+  #render() {
+    this.#root.classList.add("progress");
+    this.#root.innerHTML = Progress.createHTMLTemplate();
 
-	destroy() {
-		this.#root.classList.remove("progress");
-		this.#root.removeAttribute("style");
-		if (this.#type !== null) {
-			this.#root.classList.remove(
-				Progress.progressTypeClassnames[this.#type]
-			);
-		}
-	}
+    if (this.#type !== null) {
+      this.#root.classList.add(Progress.progressTypeClassnames[this.#type]);
+    }
+  }
+
+  #setup() {
+    this.#filler = this.#root.querySelector("[data-progress-filler]");
+
+    this.onChange(this.#value);
+  }
+
+  onChange(value) {
+    if (this.#root !== null) {
+      this.#filler.style.setProperty("width", `${value}%`);
+    }
+  }
+
+  destroy() {
+    this.#root.classList.remove("progress");
+    if (this.#type !== null) {
+      this.#root.classList.remove(Progress.progressTypeClassnames[this.#type]);
+    }
+
+    this.#root.innerHTML = "";
+  }
 }
